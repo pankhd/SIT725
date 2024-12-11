0b8e06e07d767d9f888ac917a3a80898d24b9fd0
devna <147586305+pankhd@users.noreply.github.com>
2024-12-11 13:41:09 +1100

    Week-5

---
+++ Week-5/public/js/extra.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalButton = document.querySelector('.modal-trigger');
+    const modal = document.querySelector('#modal1');
+  
+    modalButton.addEventListener('click', function() {
+      modalButton.style.backgroundColor = 'red';  
+  
+      var instance = M.Modal.init(modal); 
+      instance.open(); 
+    });
+  
+  });
+
+
+  
